089693764bd604af5a429b125136cafccf5a88de
raulanatol <raul@natol.es>
2018-05-10 20:04:54 +0100

    POST movies

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -1,6 +1,7 @@
 module.exports = {
   getMovies,
-  getMovie
+  getMovie,
+  newMovie
 };
 
 function getMovies(req, res) {
@@ -14,4 +15,12 @@ function getMovies(req, res) {
 function getMovie(req, res) {
   console.log('Buscando por la película con id', req.swagger.params.movie_uuid.value);
   res.json({ name: 'Aquí tienes tu película' });
+}
+
+function newMovie(req, res) {
+  console.log('Creando una nueva película', req.swagger.params.movie.value);
+  const newMovie = {
+    name: req.swagger.params.movie.value.name
+  };
+  res.json(newMovie);
 }
